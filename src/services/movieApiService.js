0191fbb87968be993a7cad8d697349aa60b1b5e1
Denys Cherednychenko <cherydens@ukr.net>
2023-07-09 14:35:31 +0300

    Fix

--- src/services/movieApiService.js
+++ src/services/movieApiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const TOKEN =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NTc5NmYxNzM4NzI3OTg1NWM3MjBiMjdmNDljMjVmMSIsInN1YiI6IjY0N2Y3MDYzNzFmZmRmMDEwYzg3YWIzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9AzF80qawQzmYwfptJjDK5gSiKy9M6ZEOpq2I4PFWtg';
+// const TOKEN =
+//   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NTc5NmYxNzM4NzI3OTg1NWM3MjBiMjdmNDljMjVmMSIsInN1YiI6IjY0N2Y3MDYzNzFmZmRmMDEwYzg3YWIzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9AzF80qawQzmYwfptJjDK5gSiKy9M6ZEOpq2I4PFWtg';
 const API_KEY = '55796f17387279855c720b27f49c25f1';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
